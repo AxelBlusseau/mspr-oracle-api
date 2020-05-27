b9731ea9deed3502a8190b609a2b86f28253b500
antoine burgaudeau <antoine.burgaudeau@gmail.com>
2020-05-27 09:56:35 +0200

    test corssdomain camion and demand

--- src/main/java/com/example/demo/dal/ICamionRepository.java
+++ src/main/java/com/example/demo/dal/ICamionRepository.java
@@ -4,8 +4,9 @@ package com.example.demo.dal;
 import com.example.demo.bo.Camion;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-
+@CrossOrigin
 @RepositoryRestResource(path = "camions-list", collectionResourceRel = "camions_data")
 public interface ICamionRepository extends CrudRepository<Camion, Integer> {
 }
--- src/main/java/com/example/demo/dal/IDemandeRepository.java
+++ src/main/java/com/example/demo/dal/IDemandeRepository.java
@@ -4,7 +4,9 @@ import com.example.demo.bo.Demande;
 import com.example.demo.bo.Fonction;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource(path = "demandes-list", collectionResourceRel = "demandes_data")
 public interface IDemandeRepository extends CrudRepository<Demande, Integer> {
 }
